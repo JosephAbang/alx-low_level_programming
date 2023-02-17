f50f41ba96824b7bb56a61cc23acf8cdba978c38
Joseph <abangjosef@gmail.com>
2023-02-16 19:10:06 -0800

    Print string with trailing new line without using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * print text file
  *
  * return o
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
