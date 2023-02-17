3d38fdc8be9a1ebf8dfbad1e843a4d5c64675294
Joseph <abangjosef@gmail.com>
2023-02-17 13:41:35 -0800

    Print a to z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a - z
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
