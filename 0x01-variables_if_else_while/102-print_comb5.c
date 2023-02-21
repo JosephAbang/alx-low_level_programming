4b69931580145cb38860739149bf77f0551d6193
Joseph <abangjosef@gmail.com>
2023-02-20 18:17:12 -0800

    All possible combinations of 2 digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Mirror two different combinatons of numbers
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = '0';
+		while (b <= '9')
+		{
+			c = b + 1;
+			d = a;
+			while (c <= '9')
+			{
+				while (d <= '9')
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a < '9' || b < '8' || c < '9' || d < 9)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					d++;
+				}
+				c++;
+				d = '0';
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
