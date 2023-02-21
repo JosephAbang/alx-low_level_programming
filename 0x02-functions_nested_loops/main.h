dc7eae05e71d2d560d6de1811eed0d1776584b43
Joseph <abangjosef@gmail.com>
2023-02-21 12:54:38 -0800

    Print each word from a string array using main file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,6 @@
-#include <unistd.h>
 #ifndef main_h
 #define main_h
 
-/**
- * _putchar - wirtes the character c to stdout
- * @c: The character to print
- *
- * return: On success 1.
- * ON ERROR, -1 is returned, and errno is set appopriately.
- */
-
-int main(void)
-{
-
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
-}
+int _putchar(char);
 
 #endif
