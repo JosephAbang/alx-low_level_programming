3aa7bd9f80607f881a8200ef93ba264e3c891c6c
Joseph <abangjosef@gmail.com>
2023-02-21 16:50:49 -0800

    print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,22 +15,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(44);
-		_putchar(32);
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
+		return (1);
 	}
 	else
 	{
 		_putchar(48);
-		_putchar(44);
-		_putchar(32);
 		return (0);
 	}
 }
