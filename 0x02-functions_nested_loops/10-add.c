7dfa1b1abc98fecc063f3e7582bbdfeff9569516
Joseph <abangjosef@gmail.com>
2023-02-21 19:56:53 -0800

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * description : add two numbers
+ *
+ * @i : left number to be added
+ *
+ * @j : right number to be added
+ *
+ * return : int
+ */
+
+int add(int i,int j)
+{
+	return (i + j);
+}
