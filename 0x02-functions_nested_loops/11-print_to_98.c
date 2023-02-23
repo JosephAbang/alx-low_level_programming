cd87b30f8b818b96d197e34e1e3c379dfcf6a449
Joseph <abangjosef@gmail.com>
2023-02-23 04:48:43 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,27 @@ void print_to_98(int n)
 {
 		if (n < 98)
 		{
-			for (n = n; n < 98; n++)
-				_putchar(n);	
+			while (n < 99)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ');
+				n++;
+			}
 		}
 		else if (n > 98)
 		{
-			for (n = n; n > 98; n--)
-				_putchar(n);
+			while (n > 99)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+                                _putchar(' ');
+				n--;
+			}
 		}
 		else
 		{
-			_putchar(n);
+			_putchar(n + '0');
+			_putchar('\n');
 		}
 }
