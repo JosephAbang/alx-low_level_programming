8d670509f59dbe93b5f27c471dd55b84313de61c
Joseph <abangjosef@gmail.com>
2023-02-23 20:56:31 -0800

    Print diagonal line to terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,10 +3,33 @@
 /**
  * print_diagonal - Print diagonal line
  *
+ * @n: number of times to print
+ *
  * return: void
  */
 
-int print_diagonal(int n)
+void print_diagonal(int n)
 {
+	int i, j;
 
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+			j = 0;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
