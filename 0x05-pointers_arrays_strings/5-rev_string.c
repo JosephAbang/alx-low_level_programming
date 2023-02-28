b0435c458cd0f286e83b8b0dff8f052791b40c52
Joseph <abangjosef@gmail.com>
2023-02-27 19:26:41 -0800

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - Reverses a string
+ *
+ * @s: String
+ *
+ *
+ * return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+	char temp;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+	}
+}
