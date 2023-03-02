4fd7015abfa8e366f2c217135c3e5c04b386973b
Joseph <abangjosef@gmail.com>
2023-03-02 05:48:43 -0800

    Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of string
+ *
+ * @str: String
+ *
+ * Return: capitalized char string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n' || str[i] == ',' || str[i] == '.' || str[i] == '!')
+		{
+			if ((int)str[i + 1] >= 97 && (int)str[i + 1] <= 122)
+			{
+				str[i + 1] = (int)str[i + 1] - 32;
+			}
+		}
+		else if (str[i] == '\t' || str[i] == ' ' || str[i] == '{' || str[i] == '}')
+		{
+			if ((int)str[i + 1] >= 97 && (int)str[i + 1] <= 122)
+			{
+				str[i + 1] = (int)str[i + 1] - 32;
+			}
+		}
+		else if (str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')')
+		{
+			if ((int)str[i] >= 97 && (int)str[i] <= 122)
+			{
+				str[i + 1] = (int)str[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
