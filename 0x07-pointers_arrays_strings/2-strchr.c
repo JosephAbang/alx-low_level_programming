fcdb6636ebd48656de2c01d669cc4391ade2ef51
Joseph <abangjosef@gmail.com>
2023-03-06 19:52:50 -0800

    Return pointer to the first occurence of string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s && *s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
 		s++;
 	}
-	return ((void *)0);
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return ((void *)0);
+	}
 }
