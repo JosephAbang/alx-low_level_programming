9b3f5291dc61a5d2aad81f48bf02ef3d59e82958
Joseph <abangjosef@gmail.com>
2023-03-10 14:42:31 -0800

    Print the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains C-programs that involve main fuctions and the arguments passed within them in the command line
