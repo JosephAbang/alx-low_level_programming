2adfc9eec632ad7dea834484a84d92cb411befb0
Joseph <abangjosef@gmail.com>
2023-03-22 20:01:45 -0700

    Search for an integar

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - returns ths index of first appearance of nunber
+ *
+ * @array: array
+ * @size: array size
+ * @cmp: function pointer
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
