2f5d25b57d7777a3b943549cf0a12dc95e4d8da1
Joseph <abangjosef@gmail.com>
2023-03-22 19:12:27 -0700

    Execute function for each elememt of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0;
 
-	if (size == 0 || array == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (; i < size; i++)
 	{
