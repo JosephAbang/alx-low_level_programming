b1af3e17d0c3e428d0fae03246d2fd52aa878ef1
Joseph <abangjosef@gmail.com>
2023-04-03 05:06:43 -0700

    add new node at the beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -21,5 +22,6 @@ typedef struct listint_s
 int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LIST_H */
