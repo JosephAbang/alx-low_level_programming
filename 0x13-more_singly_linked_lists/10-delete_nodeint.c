64bc3f13434d7fed9b5a9b3fcace94b2af467396
Joseph <abangjosef@gmail.com>
2023-04-03 15:50:37 -0700

    delete the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *xnode, *current;
-	unsigned int count;
+	unsigned int count = 0;
 
 	xnode = *head;
 	current = NULL;
