27e3a286ecd03996cc3c6794a41607be3be26bbb
Joseph Abang <abangjosef@gmail.com>
2023-08-17 15:43:13 -0700

    Check errors and edge cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: head of list
+ * @idx: index of node
+ * @n: data
+ * Return: the address of the new node, or NULL if it failed
+ **/
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp1 = *h, *temp2 = *h, *new_node;
@@ -8,15 +16,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (temp1)
 	{
 		temp1 = temp1->next;
-		cnt++;	
+		cnt++;
 	}
-	if (idx > cnt)
+	if (idx >= cnt)
 		return (NULL);
 	else if (idx == 0)
 		return (add_dnodeint(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 	temp1 = *h;
 	while (idx != 1)
 	{
@@ -25,14 +33,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (temp1->next == NULL)
 		return (add_dnodeint_end(h, n));
-	else
-	{
-		new_node->n = n;
-		temp2 = temp1->next;
-		new_node->prev = temp1;
-		new_node->next = temp2;
-		temp2->prev = new_node;
-		temp1->next = new_node;
-	}
+
+	new_node->n = n;
+	temp2 = temp1->next;
+	new_node->prev = temp1;
+	new_node->next = temp2;
+	temp2->prev = new_node;
+	temp1->next = new_node;
+
 	return (new_node);
 }
