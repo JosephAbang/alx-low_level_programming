da8ea605654898df6b05d26752081daa2cbd1d77
Joseph Abang <abangjosef@gmail.com>
2023-08-17 16:18:26 -0700

    Check errors and edge cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp1 = *h, *temp2 = *h, *new_node;
 	unsigned int cnt;
 	
-	if (*h == NULL)
-		return NULL;
 	while (temp1)
 	{
 		temp1 = temp1->next;
@@ -24,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	else if (idx == 0)
 		return (add_dnodeint(h, n));
-	if ((*h)->next == NULL)
+	if ((*h)->next == NULL || *h == NULL)
 		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
