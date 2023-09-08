fe5213e2f24f6ff053c18065380c32d3d206f957
Joseph Abang <abangjosef@gmail.com>
2023-09-08 15:26:28 -0700

    cast value to char

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new == NULL)
 		return (0);
 	new->key = strdup(key);
-	new->value = strdup((char *)value);
+	new->value = strdup(value);
+	if (new->value == NULL)
+		return (0)if (new->value == NULL)
+			return (0);;
 
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx] == NULL)
