abf3d3f71fe5664c900c9d212968136051181a09
JosephAbang <abangjosef@gmail.com>
2023-09-09 00:52:01 +0100

    Retrieve the value of a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrive  value
+ * @ht: hash table
+ * @key: table key
+ * Return: the value associated with the element, or NULL if key couldn’t be found
+ **/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int idx;
+
+	idx = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[idx];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return temp->value;
+		else
+			temp = temp->next;
+	}
+	return (NULL);
+}
