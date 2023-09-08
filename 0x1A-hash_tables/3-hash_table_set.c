2ec505ca33a54a09de8b40eb13a4c194b88581c3
Joseph Abang <abangjosef@gmail.com>
2023-09-08 15:29:35 -0700

    validate string duplication

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,9 +20,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	if (new->value == NULL)
-		return (0)if (new->value == NULL)
-			return (0);;
+	if (new->value == NULL || new->key == NULL)
+		return (0);
 
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx] == NULL)
