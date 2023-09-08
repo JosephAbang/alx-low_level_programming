629d0e952c385790ec20f90e76603c79b04d41ec
JosephAbang <abangjosef@gmail.com>
2023-09-08 17:18:55 +0100

    Remove allocation for table member

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,9 +4,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	hash_node_t *bucket_list = malloc(sizeof(hash_node_t) * size);
 
-	if (new_table == NULL || bucket_list == NULL)
+	if (new_table == NULL)
 		return NULL;
 	
 	new_table->size = size;
