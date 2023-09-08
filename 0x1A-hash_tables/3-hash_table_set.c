fd1e687aab47db4bc518fc015925b0b178decf5d
Joseph Abang <abangjosef@gmail.com>
2023-09-08 15:35:43 -0700

    validate hash table:

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strlen(value) == 0 || value == NULL)
 		return (0);
+	if (ht == NULL)
+		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
